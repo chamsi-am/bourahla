71c53aab74e14b24a96f4bb3b8a781d94034a724
amirarima <mariidja7@gmail.com>
2021-11-20 15:16:26 +0100

    aa

---
+++ latexbourahla/xor/amira xor.py
@@ -0,0 +1,9 @@
+import numpy
+
+def xor(x,y):
+    return bool((x and not y) or (not x and y))
+
+print(xor(0,0))
+print(xor(0,1))
+print(xor(1,0))
+print(xor(1,1))
--- latexbourahla/xor/example_xor.py
+++ latexbourahla/xor/example_xor.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-from network import Network
-from fc_layer import FCLayer
-from activation_layer import ActivationLayer
-from activations import tanh, tanh_prime
-from losses import mse, mse_prime
 
 # training data
 x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
